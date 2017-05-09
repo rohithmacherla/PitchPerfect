8d94806db9f781a0f032aed620e4eb120eb1e68c
Rohith Macherla <rohithmacherla@Rohiths-MacBook-Pro.local>
2017-05-09 18:21:37 -0400

    Added an image set, and changed the image of the record button

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -11,9 +11,16 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(<#T##animated: Bool##Bool#>)
     }
 
     override func didReceiveMemoryWarning() {
