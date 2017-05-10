a3e329928173f9b577f852e646f0e008b984c86d
Rohith Macherla <rohithmacherla1997@gmail.com>
2017-05-10 12:47:29 -0400

    Created IBOutlets for each of the buttons, and linked each button to a method that handles if they are pressed. A method for the stop button is also implemented.

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -11,10 +11,28 @@ import UIKit
 class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
 
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
